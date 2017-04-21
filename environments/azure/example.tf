647d41b797596050d84326652c36126a9d7da391
Josh Phillips <joshua.andrew.phillips@gmail.com>
2017-04-21 13:07:22 -0400

    need to finish image names

--- environments/azure/example.tf
+++ environments/azure/example.tf
@@ -54,12 +54,31 @@ resource "azurerm_network_security_group" "jenkinsPrivate" {
   }
 
   tags {
-    environment = "Production"
+    environment = "jenkins"
   }
 }
 
+resource "azure_security_group" "ssh_access" {
+	name = "ssh_access"
+	location = "East US"
+}
+
+resource "azure_security_group_rule" "ssh_access" {
+	depends_on = ["azure_security_group.testnsg"]
+	name = "my-ssh-access-rule"
+	security_group_names = ["${azure_security_group.ssh_access.name}"]
+	type = "Inbound"
+	action = "Allow"
+	priority = 200
+	source_address_prefix = "*"
+	source_port_range = "*"
+	destination_address_prefix = "*"
+	destination_port_range = "22"
+	protocol = "TCP"
+}
+
 #Create Jenkins vnet
-resource "azurerm_virtual_network" "network"{
+resource "azurerm_virtual_network" "jenkinsNetwork"{
   name          = "jenkinsNetwork"
   address_space = ["10.0.0.0/16"]
   location      = "East US"
@@ -76,4 +95,35 @@ resource "azurerm_virtual_network" "network"{
     address_prefix      = "10.0.2.0/24"
     security_group      = "${azurerm_network_security_group.jenkinsPrivate.id}"
   }
+}
+
+resource "azure_storage_service" "storage" {
+	name = "myvmstorage"
+	location = "East US"
+	description = "Made by Terraform"
+	account_type = "Standard_LRS"
+}
+
+resource "azure_instance" "jenkinsMaster" {
+	depends_on = ["azure_virtual_network.jenkinsNetwork", "azure_storage_service.storage", "azure_security_group.ssh_access", "azure_security_group.jenkinsPublic" ]
+	name = "jenkinsMaster"
+	hosted_service_name = "${azure_hosted_service.myservice.name}"
+	description = "my linux server"
+  #change this image name
+	image = "Ubuntu Server 14.04 LTS"
+	size = "Basic_A1"
+	subnet = "moduleone"
+	virtual_network = "${azure_virtual_network.jenkinsNetwork.name}"
+	storage_service_name = "${azure_storage_service.storage.name}"
+	location = "East US"
+	username = "Github123"
+	password = "Github123"
+	count = "1"
+	endpoint {
+		name = "SSH"
+		protocol = "tcp"
+		public_port = 22
+		private_port = 22
+	}
+	security_group = "${azure_security_group.ssh_access.name}"
 }
